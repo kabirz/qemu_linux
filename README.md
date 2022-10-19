e156c425957da0a0d5264d003720017a7b23edb3
kabirz <jxwazxzhp@126.com>
2022-10-19 16:19:09 +0800

    enable kgdb debug

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ linux module for qemu
 ```bash
 scp -r name@ip:GIT_ROOT/linux/out/_modules/lib/modules/linuxxxx/extra .
 ```
+
+### kgdb
+patch: patches/kdb_arm64.patch
+config: patches/kdb_config.diff
+1. [arm64 kgdb1](https://blog.csdn.net/qq_35712169/article/details/108217641) [arm64 kgdb2](https://zhuanlan.zhihu.com/p/197545583)
---
+++ kdb_virt.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+if [ ! -d out ]; then
+	mkdir out
+fi
+if [ ! -e out/rootfs_arm64.ext4 ];then
+	tar xf images/arm64.tar.xz -C out
+fi
+qemu-system-aarch64 \
+	-M virt \
+	-cpu cortex-a53 \
+	-nographic \
+	-smp 1 \
+	-kernel  linux/kdb_out/arch/arm64/boot/Image \
+	-append "rootwait root=/dev/vda console=ttyAMA0,115200 kgdboc=ttyAMA0,115200 kgdbwait nokaslr" \
+	-netdev user,id=eth0 \
+	-device virtio-net-device,netdev=eth0 \
+	-drive file=out/rootfs_arm64.ext4,if=none,format=raw,id=hd0 \
+	-device virtio-blk-device,drive=hd0 \
+	-serial tcp::1234,server,nowait
